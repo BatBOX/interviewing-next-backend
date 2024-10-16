4e4a65e58629c926a1a4befa6f7452c9d9da52fc
BatBOX <1501518204@qq.com>
2024-10-16 19:40:56 +0800

    项目结构初始化

---
+++ src/main/java/com/batbox/interviewing/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.batbox.interviewing.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.batbox.interviewing.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
